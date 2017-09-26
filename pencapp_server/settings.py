214f0fe883b6851331ffc4112ad5d94adda30cab
Esteban Bordon <ebordon@gmail.com>
2017-09-26 11:45:42 -0300

    heroku static config fix

--- pencapp_server/settings.py
+++ pencapp_server/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_FILES = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
